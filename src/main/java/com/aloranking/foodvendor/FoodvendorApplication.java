e62df3b116193bd38f69793977ab2b5b52f6f33f
aloRanking <adigunlateef02@gmail.com>
2020-05-15 21:01:09 +0100

    initial commit

---
+++ src/main/java/com/aloranking/foodvendor/FoodvendorApplication.java
@@ -0,0 +1,13 @@
+package com.aloranking.foodvendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodvendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodvendorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aloranking/foodvendor/FoodvendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aloranking.foodvendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodvendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
