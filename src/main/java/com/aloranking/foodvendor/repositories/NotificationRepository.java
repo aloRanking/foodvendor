f272ba8e0f3987ede37add3a06f21c2b1a940d8f
aloRanking <adigunlateef02@gmail.com>
2020-05-29 07:43:57 +0100

    added findBySubjectUser method

--- src/main/java/com/aloranking/foodvendor/repositories/NotificationRepository.java
+++ src/main/java/com/aloranking/foodvendor/repositories/NotificationRepository.java
@@ -1,7 +1,10 @@
 package com.aloranking.foodvendor.repositories;
 
+import com.aloranking.foodvendor.models.AuthUser;
 import com.aloranking.foodvendor.models.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    public List<Notification> findBySubjectUser(AuthUser authUser);
 }
