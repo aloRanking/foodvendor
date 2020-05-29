9b0440a5ec97aa8da7159983df843774b132ae9c
aloRanking <adigunlateef02@gmail.com>
2020-05-29 23:02:29 +0100

    added "/authenticate" endpoint

--- src/main/java/com/aloranking/foodvendor/controllers/AuthorizationController.java
+++ src/main/java/com/aloranking/foodvendor/controllers/AuthorizationController.java
@@ -1,34 +1,54 @@
 package com.aloranking.foodvendor.controllers;
 
 import com.aloranking.foodvendor.exceptions.UserNotFoundException;
-import com.aloranking.foodvendor.models.AuthUser;
-import com.aloranking.foodvendor.models.Customer;
-import com.aloranking.foodvendor.models.Password;
-import com.aloranking.foodvendor.models.Vendor;
+import com.aloranking.foodvendor.models.*;
 import com.aloranking.foodvendor.repositories.AuthUserRepository;
 import com.aloranking.foodvendor.repositories.CustomerRepository;
 import com.aloranking.foodvendor.repositories.VendorRepository;
-import com.aloranking.foodvendor.services.CustomerService;
-import com.aloranking.foodvendor.services.VendorService;
+import com.aloranking.foodvendor.services.*;
+import com.aloranking.foodvendor.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("api/v1")
 public class AuthorizationController {
 
-
-
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private AuthUserDetailService authUserDetailService;
+    @Autowired
+    private JwtUtil jwtTokenUtil;
     @Autowired
     private AuthUserRepository authUserRepository;
 
-    @GetMapping(value = "/home")
-    public String home() {
-        return ("<h1> Welcome Home</h1>");
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?>loginUser(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
+        try {
+
+         authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+        );
+        }catch (BadCredentialsException e){
+            throw new Exception("Incorrect Username or Password",e);
+        }
+        final UserDetails userDetails = authUserDetailService
+                .loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 
     @GetMapping(value = "/users")
