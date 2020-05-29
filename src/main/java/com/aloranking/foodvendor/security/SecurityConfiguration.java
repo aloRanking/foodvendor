556be151b17d20438d86c1abcf283e20dcdc9ea5
aloRanking <adigunlateef02@gmail.com>
2020-05-29 23:00:15 +0100

    added Jwt filter

--- src/main/java/com/aloranking/foodvendor/security/SecurityConfiguration.java
+++ src/main/java/com/aloranking/foodvendor/security/SecurityConfiguration.java
@@ -1,15 +1,20 @@
 package com.aloranking.foodvendor.security;
 
+import com.aloranking.foodvendor.filters.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -32,6 +37,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
@@ -39,18 +47,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/api/v1/authenticate").permitAll()
                 .antMatchers(AUTH_WHITELIST).permitAll()
-                .antMatchers("/","/login").permitAll()
-                .antMatchers("/register/customer","/register/vendor").permitAll()
-                .antMatchers("/customers/**","/vendors/**").permitAll()
-               .antMatchers("/menus", "/orders","/notification/{authUserId}/all").permitAll()
-                .antMatchers().permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable().formLogin()
-                .defaultSuccessUrl("/home");
+                .anyRequest().authenticated().and().
+                exceptionHandling().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        //.defaultSuccessUrl("/home");
+
 
 
 
